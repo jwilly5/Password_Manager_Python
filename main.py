0f02ebabc85deb4cfad35e94328fa2739a8ac85f
jwilly5 <54506189+jwilly5@users.noreply.github.com>
2022-01-01 15:25:15 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+def generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password_gen = "".join(password_list)
+
+    password_input.delete(0, END)
+    password_input.insert(0, password_gen)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+
+    website = website_input.get()
+    email = email_user_input.get()
+    password = password_input.get()
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="oops", message="Please fill out all entry fields!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nWebsite: {website} \nEmail: {email}\n Password: {password}\n Is it okay to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"\n{website} | {email} | {password}")
+            website_input.delete(0, END)
+            password_input.delete(0, END)
+            messagebox.showinfo(title="Successful", message="Your information was successfully saved!")
+        else:
+            pass
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+window.eval('tk::PlaceWindow . center')
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# labels
+website_label = Label(text="Website:              ")
+website_label.grid(row=1, column=0)
+email_user_label = Label(text="Username/Email:")
+email_user_label.grid(row=2, column=0)
+password_label = Label(text="Password:          ")
+password_label.grid(row=3, column=0)
+
+# Entry Fields
+website_input = Entry(width=38)
+website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
+email_user_input = Entry(width=38)
+email_user_input.grid(row=2, column=1, columnspan=2)
+email_user_input.insert(0, "j_wilcox_5@hotmail.com")
+password_input = Entry(width=21)
+password_input.grid(row=3, column=1)
+
+# Buttons
+generate_password = Button(text="Generate Password", command=generator)
+generate_password.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+window.mainloop()
